5a4c2a44abcba49235b5247478a26cf180c68630
gaaprado <gaa.prado@hotmail.com>
2016-08-27 14:14:44 -0300

    inclusao banco de dados

--- main.py
+++ main.py
@@ -2,47 +2,72 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui  import *
 
 
-class ImageLoad(QWidget):
-    def __init__(self, numImg):
+class Main(QWidget):
+    def __init__(self, nameImg):
         QWidget.__init__(self)
 
         image = QLabel()
-        hi = QPushButton("Configurations")
-        layout = QVBoxLayout()
+        butConfig = QPushButton("Configurations")
+        layout = QVBoxLayout()   
 
-        #Necessario alterar o path para que funcione...
-        if(numImg == 0):
-            pixmap = QPixmap('C:/Users/Prado/Desktop/FottoramaPY/resources/images/planet.jpg')
-        elif(numImg == 1):
-            pixmap = QPixmap('C:/Users/Prado/Desktop/FottoramaPY/resources/images/cat.png')
-        elif(numImg == 2):
-            pixmap = QPixmap('C:/Users/Prado/Desktop/FottoramaPY/resources/images/building.jpg')
+        if nameImg == "":
+            path = os.path.realpath("")+"/resources/images/planet.jpg"
+            pixmap = QPixmap(path)
+            image.setPixmap(pixmap)
+            
+        elif nameImg != "":
+            
+            try:
+                con    = lite.connect('fottorama.db')
+                cur    = con.execute("SELECT diretorio FROM images WHERE nome LIKE '{name}'".\
+                                     format(name = nameImg))
+                data   = cur.fetchone()
+                path   = os.path.realpath("")
+                pixmap = QPixmap(path+data[0])
+                
+                image.setPixmap(pixmap)
+                con.close()
+                
+            except ValueError:
+                print("Error sql")
 
-        image.setAlignment(Qt.AlignCenter)
-        image.setPixmap(pixmap)
+        butConfig.clicked.connect(lambda: self.selectImage())        
 
+        image.setAlignment(Qt.AlignCenter)    
+                    
         layout.addWidget(image)
-        layout.addWidget(hi)
+        layout.addWidget(butConfig)
 
         self.resize(500,500)
         self.setFixedSize(500,500)
         self.setWindowTitle("Show, Image!")
         self.setLayout(layout)
 
-        hi.clicked.connect(lambda: self.close())
-        
-        
-class Main(QWidget):
+    def selectImage(self):
+        self.close()
+        self.window = ImageSelect()
+        self.window.show()
+            
+class ImageSelect(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-
-        #layoutVertical = QGridLayout()
+        
         imgLabel       = QLabel("Select Image:", self)
         comboBox       = QComboBox(self)
-
-        imgLabel.move(50, 205)
         
-        comboBox.addItems(["planet.jpg", "cat.png", "building.jpg"]);
+        imgLabel.move(50, 205)
+
+        try:
+
+            connection = lite.connect('fottorama.db')
+            cur        = connection.execute('SELECT nome, diretorio FROM images')
+            rows       = cur.fetchall()
+            
+            comboBox.addItems([rows[0][0], rows[1][0], rows[2][0]])
+
+        except ValueError:
+            print("Error: SQL")
+
         comboBox.setMinimumWidth(200)
         comboBox.setMinimumHeight(25)
         comboBox.setMaximumWidth(200)
@@ -55,23 +80,26 @@ class Main(QWidget):
         but.setMaximumWidth(400)
         but.setMaximumHeight(20)
         
-        but.clicked.connect(lambda: self.sayHello(comboBox.currentIndex()))
+        but.clicked.connect(lambda: self.sayHello(comboBox.currentText()))
         but.move(50,250)
         
         self.resize(500, 500)
         self.setFixedSize(500,500)
         self.setWindowTitle("Show, Image!")
 
-    def sayHello(self, numero):
-        format(numero)
-        self.window = ImageLoad(numero)
+    def sayHello(self, nome):
+        format(nome)
+        self.window = Main(nome)
         self.window.show()
+        self.close()
                 
 if __name__ == "__main__":
     import sys
+    import sqlite3 as lite
+    import os
 
     app = QApplication(sys.argv)
-
-    main = Main()
+    
+    main = Main("")
     main.show()
     sys.exit(app.exec_())
